8184d49551b73209de8715e63e1e9454a1db9699
Anton D. Kachalov <mouse@yandex-team.ru>
2014-10-30 23:05:15 +0200

    Initial commit
    Signed-off-by: Anton D. Kachalov <mouse@yandex-team.ru>

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+dependency_links = []
+install_requires = []
+
+with open('requirements.txt') as f:
+    for line in f:
+        if line.startswith("#"):
+            continue
+        if '#egg=' in line:
+            dependency_links.append(line)
+            continue
+        install_requires.append(line)
+
+setup(
+    name='python-fsmgracoll',
+    version='0.1',
+    description='Finite State Machine Graphite collectors for Python',
+    author='Anton D. Kachalov',
+    packages=find_packages(),
+    platforms='any',
+    zip_safe=False,
+    include_package_data=True,
+    dependency_links=dependency_links,
+    install_requires=install_requires,
+)
