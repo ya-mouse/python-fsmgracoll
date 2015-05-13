96ab501a6d42402030693ca0f856830e6c60cde3
160382 <ya.kandid@yandex.ru>
2015-05-13 12:33:51 +0300

    Update __init__.py
    Forgotten import

--- fsmgracoll/__init__.py
+++ fsmgracoll/__init__.py
@@ -3,5 +3,6 @@ from .ipmi import IpmiUdpAgent
 from .snmp import SnmpUdpAgent
 from .ssh2 import SSHClient
 from .modbus import ModbusTcpAgent
+from .modbus import ModbusBatchAgent
 from .netcat import NetcatUdpAgent
 from .types import *
