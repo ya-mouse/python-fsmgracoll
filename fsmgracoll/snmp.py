5b00f9c798a9c898ef99bdb6817eec8b492a4f29
160382 <ya.kandid@yandex.ru>
2016-01-12 15:35:21 +0300

    Update snmp.py

--- fsmgracoll/snmp.py
+++ fsmgracoll/snmp.py
@@ -28,6 +28,7 @@ class SnmpUdpAgent(proto.SnmpUdpClient, AgentClient):
         # Run forever
         self._expire = time() + self._interval
         self._timeout = self._expire + 15.0
+        return False
 
     @staticmethod
     def _get_value(v, t):
